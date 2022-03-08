4a468039d7647725c39529f9c60ab1cf9a104746
Daniel <daniel.holmberg97@gmail.com>
2022-03-08 18:41:42 +0100

    Add PFN regressor for ch ne sv consituents

---
+++ networks/pfn_regressor.py
@@ -0,0 +1,107 @@
+from scipy.fftpack import next_fast_len
+import torch
+import torch.nn as nn
+
+
+class ParticleFlowNetwork(nn.Module):
+    r"""Parameters
+    ----------
+    input_dims : int
+        Input feature dimensions.
+    num_classes : int
+        Number of output classes.
+    layer_params : list
+        List of the feature size for each layer.
+    """
+
+    def __init__(self, 
+            input_dims, 
+            Phi_sizes,
+            use_bn=False,
+            **kwargs
+        ):
+
+        super(ParticleFlowNetwork, self).__init__(**kwargs)
+        # input bn
+        self.input_bn = nn.BatchNorm1d(input_dims) if use_bn else nn.Identity(),
+        # per-particle functions
+        phi_layers = []
+        for i in range(len(Phi_sizes)):
+            phi_layers.append(nn.Sequential(
+                nn.Conv1d(input_dims if i == 0 else Phi_sizes[i - 1], Phi_sizes[i], kernel_size=1),
+                nn.BatchNorm1d(Phi_sizes[i]) if use_bn else nn.Identity(),
+                nn.ReLU())
+            )
+        self.phi = nn.Sequential(*phi_layers)
+
+    def forward(self, x, mask=None):
+        # x: the feature vector initally read from the data structure, in dimension (N, C, P)
+        x = self.phi(x)
+        if mask is not None:
+            x *= mask.bool().float()
+        return x.sum(-1)
+
+
+class ParticleFlowNetworkRegressor(nn.Module):
+    def __init__(self, 
+        ch_input_dims, 
+        ne_input_dims, 
+        sv_input_dims, 
+        num_classes,
+        Phi_sizes=(100, 100, 128),
+        F_sizes=(100, 100, 100),
+        use_bn=False,
+        for_inference=False,
+        **kwargs
+    ):
+        super(ParticleFlowNetworkRegressor, self).__init__(**kwargs)
+
+        self.ch_pfn = ParticleFlowNetwork(ch_input_dims, Phi_sizes, use_bn)
+        self.ne_pfn = ParticleFlowNetwork(ne_input_dims, Phi_sizes, use_bn)
+        self.sv_pfn = ParticleFlowNetwork(sv_input_dims, Phi_sizes, use_bn)
+
+        # global functions
+        f_layers = []
+        for i in range(len(F_sizes)):
+            f_layers.append(nn.Sequential(
+                nn.Linear(3 * Phi_sizes[-1] if i == 0 else F_sizes[i - 1], F_sizes[i]),
+                nn.ReLU())
+            )
+        f_layers.append(nn.Linear(F_sizes[-1], num_classes))
+        if for_inference:
+            f_layers.append(nn.Softmax(dim=1))
+        self.fc = nn.Sequential(*f_layers)
+
+    def forward(self, ch_features, ch_mask, ne_features, ne_mask, sv_features, sv_mask):
+        ch_x = self.ch_pfn(ch_features, ch_mask)
+        ne_x = self.ne_pfn(ne_features, ne_mask)
+        sv_x = self.sv_pfn(sv_features, sv_mask)
+        x = torch.cat((ch_x, ne_x, sv_x), dim=1)
+        return self.fc(x)
+
+
+def get_model(data_config, **kwargs):
+    Phi_sizes = (100, 100, 128)
+    F_sizes = (100, 100, 100)
+    ch_input_dims = len(data_config.input_dicts['ch_features'])
+    ne_input_dims = len(data_config.input_dicts['ne_features'])
+    sv_input_dims = len(data_config.input_dicts['sv_features'])
+    num_classes = 1
+    model = ParticleFlowNetworkRegressor(
+        ch_input_dims, ne_input_dims, sv_input_dims, num_classes, 
+        Phi_sizes=Phi_sizes, F_sizes=F_sizes, use_bn=kwargs.get('use_bn', False)
+    )
+
+    model_info = {
+        'input_names': list(data_config.input_names),
+        'input_shapes': {k: ((1,) + s[1:]) for k, s in data_config.input_shapes.items()},
+        'output_names': ['softmax'],
+        'dynamic_axes': {**{k: {0: 'N', 2: 'n_' + k.split('_')[0]} for k in data_config.input_names}, **{'softmax': {0: 'N'}}},
+    }
+
+    print(model, model_info)
+    return model, model_info
+
+
+def get_loss(data_config, **kwargs):
+    return torch.nn.SmoothL1Loss(beta=kwargs.get('loss_beta', 10))
